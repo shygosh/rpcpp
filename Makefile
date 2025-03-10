4f199e6ffa5ed0fbe4e5ecfb1181c76537e8d02b
shygosh <shygosh@proton.me>
2025-03-11 01:16:30 +0700

    Revamp

--- Makefile
+++ Makefile
@@ -1,25 +1,23 @@
-CC=/usr/bin/g++
+CC = g++
+CPPFILES = $(wildcard src/*.cpp)
+LIBFILES = $(wildcard src/discord/*.cpp)
+LDLAGS = -Llib/ -l:discord_game_sdk.so -lpthread
+CXXFLAGS = -O3 -march=native -mtune=native
 
-CPPFILES=$(wildcard src/*.cpp)
-HPPFILES=$(wildcard src/*.hpp)
-LIBFILES=$(wildcard src/discord/*.cpp)
-CFLAGS=-Llib/ -l:discord_game_sdk.so -lpthread -lX11
-
-build/rpcpp: $(CPPFILES) $(HPPFILES)
-	mkdir -p build
-	$(CC) $(CPPFILES) $(LIBFILES) $(CFLAGS) -o $@
+.PHONY: clean build install uninstall
 
 clean:
-	rm -rf tmp build
-
-install: build/rpcpp
-	mkdir -p ${DESTDIR}${PREFIX}/bin
-	mkdir -p ${DESTDIR}${PREFIX}/lib
-	cp -f build/rpcpp ${DESTDIR}${PREFIX}/bin
-	cp -f lib/discord_game_sdk.so ${DESTDIR}${PREFIX}/lib
-	chmod 755 ${DESTDIR}${PREFIX}/bin/rpcpp
+	rm -rf build
 
 uninstall:
-	rm -f ${DESTDIR}${PREFIX}/bin/rpcpp
+	rm -f $(DESTDIR)$(PREFIX)/bin/rpcpp
 
-.PHONY: clean install uninstall
+build: $(CPPFILES)
+	mkdir -p build
+	$(CC) $(CXXFLAGS) $(CPPFILES) $(LIBFILES) $(CFLAGS) -o build/rpcpp $(LDLAGS)
+
+install:
+	cp -f build/rpcpp $(DESTDIR)$(PREFIX)/bin
+	cp -f lib/discord_game_sdk.so $(DESTDIR)$(PREFIX)/lib
+	cp -f lib/discord_game_sdk.so $(DESTDIR)$(PREFIX)/lib64
+	chmod 755 $(DESTDIR)$(PREFIX)/bin/rpcpp
--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,277 @@
-#include "rpcpp.hpp"
+#include <csignal>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <time.h>
+#include <thread>
+#include <unistd.h>
 
-void *updateRPC(void *ptr)
+// Discord RPC
+#include "discord/discord.h"
+
+using namespace std;
+namespace fs = filesystem;
+
+/*
+ * Change with your own app's id if you made one
+ * Default: 934099338374824007
+ */
+#define DISCORD_APP_ID 1334839099227635772
+#define VERSION "3.0"
+
+long uptime();
+unsigned long cpuUsage();
+long memoryUsage();
+
+volatile bool shouldExit;
+string quoteBucket;
+string activityBucket;
+
+// distro names in /etc/lsb_release
+map<string, string> distros_lsb = {{"Arch|Artix", "archlinux"},
+                                   {"LinuxMint", "lmint"},
+                                   {"Gentoo", "gentoo"},
+                                   {"Ubuntu", "ubuntu"},
+                                   {"ManjaroLinux", "manjaro"}};
+// same but in /etc/os-release (fallback)
+map<string, string> distros_os = {{"Arch Linux", "archlinux"},
+                                  {"Linux Mint", "lmint"},
+                                  {"Gentoo", "gentoo"},
+                                  {"Ubuntu", "ubuntu"},
+                                  {"Manjaro Linux", "manjaro"}};
+string helpMsg = string("Usage:\n") + " rpcpp [options]\n\n" + "Options:\n" +
+                 " --interval=3           rich presence update interval in second(s)\n" +
+                 " -h, --help             display this help and exit\n" +
+                 " -v, --version          output version number and exit";
+
+regex processRegex("\\/proc\\/\\d+\\/cmdline");
+regex updateRegex("^interval=(\\d+)$");
+
+vector<pair<regex, string>> distros_lsb_regex = {};
+vector<pair<regex, string>> distros_os_regex = {};
+
+regex memavailr("MemAvailable: +(\\d+) kB");
+regex memtotalr("MemTotal: +(\\d+) kB");
+
+struct DiscordState {
+    discord::User currentUser;
+    unique_ptr<discord::Core> core;
+};
+
+struct InfoAsset {
+    string image;
+    string text;
+};
+
+struct Config {
+    int interval = 3;
+    bool printHelp = false;
+    bool printVersion = false;
+};
+
+Config config;
+
+discord::Activity g_activity{};
+
+inline __attribute__((always_inline))
+void setActivity(DiscordState &state, string *sstate)
 {
-    string windowName, lastWindow;
-    WindowAsset windowAsset;
-    DistroAsset distroAsset;
-    DiscordState *state = (struct DiscordState *)ptr;
+    g_activity.SetState(sstate->c_str());
+    state.core->ActivityManager().UpdateActivity(g_activity, [](discord::Result result) {});
+}
 
-    log("Waiting for usages to load...", LogType::DEBUG);
+void setQuote(DiscordState &state, string *details)
+{
+    g_activity.SetDetails(details->c_str());
+    state.core->ActivityManager().UpdateActivity(g_activity, [](discord::Result result) {});
+}
 
-    // wait for usages to load
-    while (cpu == -1 || mem == -1)
-    {
-        usleep(1000);
-    }
+void parseConfigOption(Config *config, char *option, bool arg)
+{
+    smatch matcher;
+    string s = option;
 
-    log("Starting RPC loop.", LogType::DEBUG);
-    distroAsset = getDistroAsset(distro);
-
-    while (true)
-    {
-        string cpupercent = to_string((long)cpu);
-        string rampercent = to_string((long)mem);
-
-        usleep(config.updateSleep * 1000);
-
-        if (!config.noSmallImage)
-        {
-            try
-            {
-                windowName = getActiveWindowClassName(disp);
-            }
-            catch (exception ex)
-            {
-                log(ex.what(), LogType::ERROR);
-                continue;
-            }
-
-            if (windowName != lastWindow)
-            {
-                windowAsset = getWindowAsset(windowName);
-                lastWindow = windowName;
-            }
+    if (arg) {
+        if (s == "-h" || s == "--help") {
+            config->printHelp = true;
+            return;
         }
 
-        setActivity(*state, string("CPU: " + cpupercent + "% | RAM: " + rampercent + "%"), "WM: " + wm, windowAsset.image, windowAsset.text, distroAsset.image, distroAsset.text, startTime, discord::ActivityType::Playing);
+        if (s == "-v" || s == "--version") {
+            config->printVersion = true;
+            return;
+        }
+
+        if (!strncmp(option, "--", 2)) {
+            s = s.substr(2, s.size() - 2);
+        }
+    }
+
+    if (regex_search(s, matcher, updateRegex)) {
+        config->interval = stoi(matcher[1]);
+        return;
     }
 }
 
-void *updateUsage(void *ptr)
+void parseConfig(string configFile, Config *config)
 {
-    distro = getDistro();
-    log("Distro: " + distro, LogType::DEBUG);
-
-    startTime = time(0) - ms_uptime();
-    wm = string(wm_info(disp));
-    log("WM: " + wm, LogType::DEBUG);
-
-    while (true)
-    {
-        mem = getRAM();
-        cpu = getCPU();
-        sleep(config.usageSleep / 1000.0);
+    ifstream file(configFile);
+    if (file.is_open()) {
+        string line;
+        while (getline(file, line)) {
+            parseConfigOption(config, (char *)line.c_str(), false);
+        }
+        file.close();
     }
+}
+
+/**
+ * @brief Parse default configs
+ * /etc/rpcpp/config < ~/.config/rpcpp/config
+ */
+void parseConfigs()
+{
+    char *home = getenv("HOME");
+    if (!home) {
+        parseConfig("/etc/rpcpp/config", &config);
+        return;
+    }
+
+    string configFile = string(home) + "/.config/rpcpp/config";
+    parseConfig(configFile, &config);
+    if (ifstream(configFile).fail()) {
+        parseConfig("/etc/rpcpp/config", &config);
+    }
+}
+
+void parseArgs(int argc, char **argv)
+{
+    for (int i = 1; i < argc; i++) {
+        parseConfigOption(&config, argv[i], true);
+    }
+}
+
+string getDistro()
+{
+    string distro = "";
+    string line;
+    ifstream release;
+    regex distroreg;
+    smatch distromatcher;
+    if (fs::exists("/etc/lsb-release")) {
+        distroreg = regex("DISTRIB_ID=\"?([a-zA-Z0-9 ]+)\"?");
+        release.open("/etc/lsb-release");
+    } else if (fs::exists("/etc/os-release")) {
+        distroreg = regex("NAME=\"?([a-zA-Z0-9 ]+)\"?");
+        release.open("/etc/os-release");
+    } else {
+        return distro;
+    }
+    while (getline(release, line)) {
+        if (regex_search(line, distromatcher, distroreg)) {
+            distro = distromatcher[1];
+            break;
+        }
+    }
+    return distro;
+}
+
+InfoAsset getDistroAsset(string d)
+{
+    InfoAsset dist{};
+    dist.text = d + " / rpcpp ";
+    dist.image = "tux";
+
+    for (const auto &kv : distros_lsb_regex) {
+        regex r = kv.first;
+        smatch m;
+        if (regex_match(d, m, r)) {
+            dist.image = kv.second;
+            break;
+        }
+    }
+    if (dist.image == "tux") {
+        for (const auto &kv : distros_os_regex) {
+            regex r = kv.first;
+            smatch m;
+            if (regex_match(d, m, r)) {
+                dist.image = kv.second;
+                break;
+            }
+        }
+    }
+
+    return dist;
+}
+
+/**
+ * @brief Compile strings to regular expressions
+ */
+void compileRegexes(map<string, string> *from, vector<pair<regex, string>> *to)
+{
+
+    for (const auto &kv : *from) {
+        const regex r = regex(kv.first);
+        to->push_back({r, kv.second});
+    }
+}
+
+/**
+ * @brief Compile all strings to regular expressions
+ */
+void compileAllRegexes()
+{
+    compileRegexes(&distros_lsb, &distros_lsb_regex);
+    compileRegexes(&distros_os, &distros_os_regex);
+}
+
+void *updateRPC(void *ptr)
+{
+    DiscordState *state = (struct DiscordState *)ptr;
+
+    while (true) {
+        activityBucket = "CPU: " + to_string(cpuUsage()) + "% | " +
+                         "RAM: " + to_string(memoryUsage() >> 10) + "MB";
+        setActivity(*state, &activityBucket);
+        this_thread::sleep_for(chrono::seconds(config.interval));
+    }
+
+    return nullptr;
+}
+
+void *updateQuote(void *ptr)
+{
+    DiscordState *state = (struct DiscordState *)ptr;
+    static char buffer[256]{};
+    static const char *cmd = "curl -s https://zenquotes.io/api/random | jq -r '.[] | \"\\(.q) — \\(.a)\"'";
+
+    sleep(3);
+    while (true) {
+        FILE *f = popen(cmd, "r");
+        if (!f) {
+            this_thread::sleep_for(chrono::seconds(30));
+            continue;
+        }
+
+        if (fgets(buffer, sizeof(buffer), f))
+            quoteBucket = buffer;
+
+        pclose(f);
+
+        if (quoteBucket.empty()) {
+            this_thread::sleep_for(chrono::seconds(30));
+            continue;
+        } else {
+            if (quoteBucket.back() == '\n')
+                quoteBucket.pop_back();
+        }
+
+        setQuote(*state, &quoteBucket);
+        this_thread::sleep_for(chrono::minutes(30));
+    }
+
+    return nullptr;
 }
 
 int main(int argc, char **argv)
@@ -70,89 +279,127 @@ int main(int argc, char **argv)
     parseConfigs();
     parseArgs(argc, argv);
 
-    if (config.printHelp)
-    {
+    if (config.printHelp) {
         cout << helpMsg << endl;
         exit(0);
     }
-    if (config.printVersion)
-    {
+    if (config.printVersion) {
         cout << "RPC++ version " << VERSION << endl;
         exit(0);
     }
 
-    int waitedTime = 0;
-    while (!processRunning("discord") && !config.ignoreDiscord)
-    {
-        if (waitedTime > 60)
-        {
-            log(string("Discord is not running for ") + to_string(waitedTime) + " seconds. Maybe ignore Discord check with --ignore-discord or -f?", LogType::INFO);
-        }
-        log("Waiting for Discord...", LogType::INFO);
-        waitedTime += 5;
-        sleep(5);
-    }
+    auto shouldWait{[](const char *name) -> int {
+        char cmd[32];
+        snprintf(cmd, sizeof(cmd), "pgrep -if %s", name);
+        FILE *f{popen(cmd, "r")};
+        if (!f)
+            return true;
+        char buffer[16];
+        fgets(buffer, sizeof(buffer), f);
+        pclose(f);
+        return buffer[0] == '\0';
+    }};
 
-    disp = XOpenDisplay(NULL);
+    while (shouldWait("discord") && shouldWait("vesktop"))
+        sleep(60);
+    sleep(5);
 
-    if (!disp)
-    {
-        cout << "Can't open display" << endl;
-        return -1;
-    }
-
-    static int (*old_error_handler)(Display *, XErrorEvent *);
-    trapped_error_code = 0;
-    old_error_handler = XSetErrorHandler(error_handler);
-
-    // Compile all regexes
     compileAllRegexes();
-
-    pthread_t updateThread;
-    pthread_t usageThread;
-    pthread_create(&usageThread, 0, updateUsage, 0);
-    log("Created usage thread", LogType::DEBUG);
 
     DiscordState state{};
 
     discord::Core *core{};
-    auto result = discord::Core::Create(934099338374824007, DiscordCreateFlags_Default, &core); // change with your own app's id if you made one
+    auto result = discord::Core::Create(DISCORD_APP_ID, DiscordCreateFlags_Default, &core);
     state.core.reset(core);
-    if (!state.core)
-    {
-        cout << "Failed to instantiate discord core! (err " << static_cast<int>(result)
-             << ")\n";
+    if (!state.core) {
+        cout << "Failed to instantiate discord core! (err " << static_cast<int>(result) << ")\n";
         exit(-1);
     }
 
-    if (config.debug)
-    {
-        state.core->SetLogHook(
-            discord::LogLevel::Debug, [](discord::LogLevel level, const char *message)
-            { cerr << "Log(" << static_cast<uint32_t>(level) << "): " << message << "\n"; });
-    }
+    InfoAsset distroAsset = getDistroAsset(getDistro());
+    g_activity.GetAssets().SetLargeImage(distroAsset.image.c_str());
+    g_activity.GetAssets().SetLargeText(distroAsset.text.c_str());
+    g_activity.GetAssets().SetSmallImage("");
+    g_activity.GetAssets().SetSmallText("");
+    g_activity.GetTimestamps().SetStart(static_cast<long>(time(nullptr) - uptime()));
+    g_activity.SetType(discord::ActivityType::Playing);
 
-    pthread_create(&updateThread, 0, updateRPC, ((void *)&state));
-    log("Threads started.", LogType::DEBUG);
-    log("Xorg version " + to_string(XProtocolVersion(disp)), LogType::DEBUG); // this is kinda dumb to do since it shouldn't be anything else other than 11, but whatever
-    log("Connected to Discord.", LogType::INFO);
+    pthread_t updateRPC_thread;
+    pthread_create(&updateRPC_thread, 0, updateRPC, ((void *)&state));
 
-    signal(SIGINT, [](int)
-           { interrupted = true; });
+    pthread_t updateQuote_thread;
+    pthread_create(&updateQuote_thread, 0, updateQuote, ((void *)&state));
 
-    do
-    {
+    signal(SIGINT, [](int) { shouldExit = true; });
+    signal(SIGPIPE, [](int) { shouldExit = true; });
+    signal(SIGTERM, [](int) { shouldExit = true; });
+
+    do {
         state.core->RunCallbacks();
-
-        this_thread::sleep_for(chrono::milliseconds(16));
-    } while (!interrupted);
+        sleep(config.interval);
+    } while (!shouldExit);
 
     cout << "Exiting..." << endl;
 
-    XCloseDisplay(disp);
-
-    pthread_kill(updateThread, 9);
-    pthread_kill(usageThread, 9);
+    pthread_kill(updateRPC_thread, 9);
+    pthread_kill(updateQuote_thread, 9);
 
     return 0;
+}
+
+long uptime()
+{
+    static char buffer[256]{};
+    long ret{};
+    FILE *f = fopen("/proc/uptime", "r");
+    if (f) {
+        if (fgets(buffer, sizeof(buffer), f))
+            ret = atol(buffer);
+        fclose(f);
+    }
+    return ret;
+}
+
+long memoryUsage()
+{
+    long ramtotal{}, ramavail{};
+    smatch matcher;
+    string line;
+    ifstream meminfo;
+    meminfo.open("/proc/meminfo");
+    while (getline(meminfo, line)) {
+        if (ramtotal && ramavail)
+            break;
+        else if (regex_search(line, matcher, memavailr))
+            ramavail = stoi(matcher[1]);
+        else if (regex_search(line, matcher, memtotalr))
+            ramtotal = stoi(matcher[1]);
+    }
+    meminfo.close();
+
+    return ramtotal - ramavail;
+}
+
+unsigned long cpuUsage()
+{
+    static unsigned long userP = 0, niceP = 0, systemP = 0, idleP = 0;
+    unsigned long user, nice, system, idle;
+
+    FILE *file = fopen("/proc/stat", "r");
+    if (!file) return 0;
+    fscanf(file, "cpu %llu %llu %llu %llu", &user, &nice, &system, &idle);
+    fclose(file);
+
+    unsigned long totalDiff = (user - userP) + (nice - niceP) +
+                              (system - systemP) + (idle - idleP);
+
+    unsigned long usage = totalDiff ? 100 * ((user - userP) + (nice - niceP) +
+                          (system - systemP)) / totalDiff : 0;
+
+    userP = user;
+    niceP = nice;
+    systemP = system;
+    idleP = idle;
+
+    return usage;
 }
